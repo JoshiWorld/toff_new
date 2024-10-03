3e9add2b98b9b7a7e56b5e45169d890127154603
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-03 19:43:00 +0200

    feature: faqs can now be edited

--- components/admin/faq.tsx
+++ components/admin/faq.tsx
@@ -1,42 +1,53 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 import { cn } from "@/lib/utils";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { FAQ } from "@/types/mongodb";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
-const FAQs = [
-  {
-    question: "What is the purpose of this website?",
-    answer:
-      "This website is a place to help you find the best products and services in the world.",
-  },
-  {
-    question: "How do I contact support?",
-    answer:
-      "You can contact support by email at support@example.com or by phone at 123-456-7890.",
-  },
-  {
-    question: "How do I find the best products?",
-    answer:
-      "You can find the best products by searching for them on the search page or by browsing the categories.",
-  },
-  {
-    question: "Can I return a product?",
-    answer:
-      "Yes, you can return a product within 30 days of purchase. Please refer to our return policy for more details.",
-  },
-  {
-    question: "Do you offer international shipping?",
-    answer:
-      "Yes, we offer international shipping to most countries. Shipping fees and delivery times may vary depending on the destination.",
-  },
-  {
-    question: "How can I track my order?",
-    answer:
-      "You can track your order by logging into your account and visiting the order history page. You will also receive a tracking number via email once your order has shipped.",
-  },
-];
 export function AdminFAQTable() {
+  const router = useRouter();
+  const [question, setQeustion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [data, setData] = useState<FAQ[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/public/faq").then((res) => res.json()).then((data) => {
+      setData(data);
+      setLoading(false);
+    })
+  }, []);
+
+  if(isLoading) return <p>Loading...</p>;
+
+  const createFAQ = async () => {
+    const token = localStorage.getItem("token");
+
+    const res = await fetch("/api/protected/faq", {
+      method: "POST",
+      body: JSON.stringify({
+        question: question,
+        answer: answer,
+      }),
+      headers: {
+        Authorization: `${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.status === 200) {
+      setQeustion("");
+      setAnswer("");
+      router.push("/admin/dashboard");
+    }
+  };
+
   return (
     <div className="mx-auto grid w-full max-w-7xl gap-4 px-4 py-20 md:px-8 md:py-40">
       <h2 className="text-center text-4xl font-medium tracking-tight text-neutral-600 dark:text-neutral-50 md:text-5xl">
@@ -46,79 +57,159 @@ export function AdminFAQTable() {
         Hier kannst du deine FAQs bearbeiten
       </p>
       <div className="mx-auto mt-10 w-full max-w-3xl">
-        {FAQs.map((faq, index) => (
+        {data.map((faq, index) => (
           <FAQItem
             key={index}
             question={faq.question}
             answer={faq.answer}
+            id={faq._id}
           />
         ))}
       </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Erstellen</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="question">Frage</Label>
+            <Input
+              type="text"
+              id="question"
+              placeholder="Frage"
+              value={question}
+              onChange={(e) => setQeustion(e.target.value)}
+            />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="answer">Antwort</Label>
+            <Input
+              type="text"
+              id="answer"
+              placeholder="Antwort"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+          </div>
+          <Button onClick={createFAQ}>Erstellen</Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
 
 const FAQItem = ({
+  id,
   question,
   answer,
 }: {
+  id: string;
   question: string;
   answer: string;
 }) => {
+  const router = useRouter();
+  const [questionNew, setQeustionNew] = useState(question);
+  const [answerNew, setAnswerNew] = useState(answer);
+
+  const updateFAQ = async () => {
+    const token = localStorage.getItem("token");
+
+    const res = await fetch("/api/protected/faq", {
+      method: "PUT",
+      body: JSON.stringify({
+        _id: id,
+        question: questionNew,
+        answer: answerNew,
+      }),
+      headers: {
+        Authorization: `${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.status === 200) {
+      router.push("/admin/dashboard");
+    }
+  };
 
   return (
-    <div
-      className="mb-8 w-full cursor-pointer rounded-lg bg-white p-4 shadow-input dark:bg-neutral-900"
-      onClick={() => {
-        
-      }}
-    >
-      <div className="flex items-start">
-        <div className="relative mr-4 mt-1 h-6 w-6 flex-shrink-0">
-          <IconChevronUp
-            className={cn(
-              "absolute inset-0 h-6 w-6 transform text-black transition-all duration-200 dark:text-white",
-              "rotate-90 scale-0"
-            )}
-          />
-          <IconChevronDown
-            className={cn(
-              "absolute inset-0 h-6 w-6 rotate-90 scale-0 transform text-black transition-all duration-200 dark:text-white",
-              "rotate-0 scale-100"
-            )}
-          />
-        </div>
-        <div>
-          <h3 className="text-lg font-medium text-neutral-700 dark:text-neutral-200">
-            {question}
-          </h3>
-          <AnimatePresence mode="wait">
-            <motion.div
-              initial={{ height: 0 }}
-              animate={{ height: "auto" }}
-              exit={{ height: 0 }}
-              transition={{ duration: 0.2, ease: "easeOut" }}
-              className="overflow-hidden text-neutral-500 dark:text-neutral-400"
-            >
-              {answer.split("").map((line, index) => (
-                <motion.span
-                  initial={{ opacity: 0, filter: "blur(5px)" }}
-                  animate={{ opacity: 1, filter: "blur(0px)" }}
-                  exit={{ opacity: 0, filter: "blur(0px)" }}
-                  transition={{
-                    duration: 0.2,
-                    ease: "easeOut",
-                    delay: index * 0.005,
-                  }}
-                  key={index}
+    <Dialog>
+      <DialogTrigger asChild>
+        <div
+          className="mb-8 w-full cursor-pointer rounded-lg bg-white p-4 shadow-input dark:bg-neutral-900"
+          onClick={() => {}}
+        >
+          <div className="flex items-start">
+            <div className="relative mr-4 mt-1 h-6 w-6 flex-shrink-0">
+              <IconChevronUp
+                className={cn(
+                  "absolute inset-0 h-6 w-6 transform text-black transition-all duration-200 dark:text-white",
+                  "rotate-90 scale-0"
+                )}
+              />
+              <IconChevronDown
+                className={cn(
+                  "absolute inset-0 h-6 w-6 rotate-90 scale-0 transform text-black transition-all duration-200 dark:text-white",
+                  "rotate-0 scale-100"
+                )}
+              />
+            </div>
+            <div>
+              <h3 className="text-lg font-medium text-neutral-700 dark:text-neutral-200">
+                {question}
+              </h3>
+              <AnimatePresence mode="wait">
+                <motion.div
+                  initial={{ height: 0 }}
+                  animate={{ height: "auto" }}
+                  exit={{ height: 0 }}
+                  transition={{ duration: 0.2, ease: "easeOut" }}
+                  className="overflow-hidden text-neutral-500 dark:text-neutral-400"
                 >
-                  {line}
-                </motion.span>
-              ))}
-            </motion.div>
-          </AnimatePresence>
+                  {answer.split("").map((line, index) => (
+                    <motion.span
+                      initial={{ opacity: 0, filter: "blur(5px)" }}
+                      animate={{ opacity: 1, filter: "blur(0px)" }}
+                      exit={{ opacity: 0, filter: "blur(0px)" }}
+                      transition={{
+                        duration: 0.2,
+                        ease: "easeOut",
+                        delay: index * 0.005,
+                      }}
+                      key={index}
+                    >
+                      {line}
+                    </motion.span>
+                  ))}
+                </motion.div>
+              </AnimatePresence>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </DialogTrigger>
+      <DialogContent>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="question">Frage</Label>
+          <Input
+            type="text"
+            id="question"
+            placeholder="Frage"
+            value={questionNew}
+            onChange={(e) => setQeustionNew(e.target.value)}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="answer">Antwort</Label>
+          <Input
+            type="text"
+            id="answer"
+            placeholder="Antwort"
+            value={answerNew}
+            onChange={(e) => setAnswerNew(e.target.value)}
+          />
+        </div>
+        <Button onClick={updateFAQ}>Speichern</Button>
+      </DialogContent>
+    </Dialog>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,17 +14,52 @@ const config: Config = {
     extend: {
       colors: {
         charcoal: "#08090A",
-        secondary: "#be0ce7",
-        muted: "var(--neutral-200)",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       boxShadow: {
-        derek: `0px 0px 0px 1px rgb(0 0 0 / 0.06),
-        0px 1px 1px -0.5px rgb(0 0 0 / 0.06),
-        0px 3px 3px -1.5px rgb(0 0 0 / 0.06), 
-        0px 6px 6px -3px rgb(0 0 0 / 0.06),
-        0px 12px 12px -6px rgb(0 0 0 / 0.06),
-        0px 24px 24px -12px rgb(0 0 0 / 0.06)`,
-        aceternity: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+        derek:
+          "`0px 0px 0px 1px rgb(0 0 0 / 0.06),\n        0px 1px 1px -0.5px rgb(0 0 0 / 0.06),\n        0px 3px 3px -1.5px rgb(0 0 0 / 0.06), \n        0px 6px 6px -3px rgb(0 0 0 / 0.06),\n        0px 12px 12px -6px rgb(0 0 0 / 0.06),\n        0px 24px 24px -12px rgb(0 0 0 / 0.06)`",
+        aceternity:
+          "`0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -35,9 +71,18 @@ const config: Config = {
       },
       keyframes: {
         move: {
-          "0%": { transform: "translateX(-200px)" },
-          "100%": { transform: "translateX(200px)" },
+          "0%": {
+            transform: "translateX(-200px)",
+          },
+          "100%": {
+            transform: "translateX(200px)",
+          },
         },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
