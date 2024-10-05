dd5ee5717f7998171d9a240e09c05c468026f7c0
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-05 12:21:12 +0200

    fix: removed serverside props

--- app/(marketing)/admin/faq/page.tsx
+++ app/(marketing)/admin/faq/page.tsx
@@ -1,17 +1,9 @@
 import { AdminFAQTable } from "@/components/admin/faq";
-import { GetServerSideProps } from "next";
 import prisma from "@/lib/prisma";
 import { FAQ } from "@prisma/client";
 
-export default function AdminFAQ({ faqs }: { faqs: FAQ[] }) {
+export default async function AdminFAQ() {
+  const faqs: FAQ[] = await prisma.fAQ.findMany();
+
   return <AdminFAQTable data={faqs} />;
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const faqs = await prisma.fAQ.findMany();
-  return {
-    props: {
-      faqs,
-    },
-  };
-};
