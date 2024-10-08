0e5de859717d1155b167085b3c5d0ed65d7751ad
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-08 11:57:44 +0200

    removed unused imports

--- app/api/protected/live/route.ts
+++ app/api/protected/live/route.ts
@@ -1,16 +1,5 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { S3Client } from "@aws-sdk/client-s3";
-
-const s3 = new S3Client({
-  region: process.env.S3_REGION,
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-  },
-});
-
-const BUCKET_NAME = process.env.S3_BUCKET_NAME!;
 
 // Create Live
 export async function POST(req: Request) {
